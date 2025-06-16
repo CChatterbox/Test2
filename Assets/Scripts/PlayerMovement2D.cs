f4522a8a6a47c0b3ddab7e1c80bbe6c23f1283fc
CChatterbox <novayapothtanormalna@gmail.com>
2025-06-16 18:02:47 +0300

    2Update
    Добавил патроны

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMovement2D : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class PlayerMovement2D : MonoBehaviour
     public GameObject firePoint;
     public GameObject bullet;
     public float bulletSpeed = 10f; 
+
+    public TMP_Text ammo;
+    private int ammoCount = 30;
     
     public Joystick joystick;
     
@@ -33,7 +37,7 @@ public class PlayerMovement2D : MonoBehaviour
        if(movement.x > 0)
        transform.localScale = new Vector3(5, 5, 5);
       
-
+        ammo.text = "Патроны " + ammoCount;
         
 
     }
@@ -44,9 +48,12 @@ rb.velocity = movement * speed;
 
 
     public void Shot()
+    { 
+        if(ammoCount > 0) 
     {
         GameObject newBullet = Instantiate(bullet, firePoint.transform.position, Quaternion.Euler(0, 0, 90));
          float direction = 1f; 
+         ammoCount -= 1;
         if (transform.localScale.x < 0)
         {
             direction = -1f; 
@@ -55,7 +62,7 @@ rb.velocity = movement * speed;
 
         Rigidbody2D rbBullet = newBullet.GetComponent<Rigidbody2D>(); 
         rbBullet.velocity = new Vector2(direction * bulletSpeed, 0f); 
-
+    }
     }
 
     
